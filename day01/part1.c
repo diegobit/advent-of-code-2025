a498516d9a316eea9fce5b6c4502925da434f8a6
Diego Giorgini <diegobit@icloud.com>
2025-12-03 21:11:15 +0100

    add day01

---
+++ day01/part1.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct dial {
+    int tick;
+    int len;
+} dial;
+
+
+void rotate(dial *d, int rotation) {
+    d->tick = d->tick + rotation;
+    while (d->tick < 0) d->tick = d->tick + d->len;
+    while (d->tick >= d->len) d->tick = d->tick - d->len;
+}
+
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+        return 1;
+    }
+
+    // Open file
+    FILE *fp = fopen(argv[1], "r");
+    if (fp == NULL) {
+        perror("File does not exist.");
+        return 1;
+    }
+
+    // Allocate malloc
+    dial *d = malloc(sizeof(*d));
+    d->tick = 50;
+    d->len = 100;
+    int zero_count = 0;
+
+    // Read file line by line
+    char buf[1024];
+    while (fgets(buf, sizeof(buf), fp) != NULL) {
+        if (buf[0] == 'L') buf[0] = '-';
+        if (buf[0] == 'R') buf[0] = '+';
+        char *end;
+        int rotation = strtol(buf, &end, 10);
+
+        // Rotate
+        // printf("ROT %d, c=%d, FROM %d to ", rotation, zero_count, d->tick);
+        rotate(d, rotation);
+        // printf("%d\n", d->tick);
+        if (d->tick == 0) zero_count++;
+    }
+
+    printf("END d->tick=%d, d->len=%d, zero_count=%d\n", d->tick, d->len, zero_count);
+}
---
+++ day01/part2.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+typedef struct dial {
+    int tick;
+    int len;
+} dial;
+
+
+/* Simulate the click over the dial. Click logic is somewhat separated from
+ * rotation. */
+int click(dial *d) {
+    if (d->tick == 0) return 1;
+    return 0;
+}
+
+/* Rotate the dial by 'rotation' number of clicks. Simulate the dial by doing
+ * the rotation one position at a time.
+ * Click logic is separated from rotation. */
+int rotate(dial *d, int rotation) {
+    int turns = 0;
+
+    int dir = rotation >= 0 ? +1 : -1;
+    rotation = abs(rotation);
+    while (rotation > 0) {
+        d->tick += dir;
+        if (d->tick >= d->len) d->tick-=d->len;
+        if (d->tick < 0)       d->tick+=d->len;
+
+        turns += click(d);
+
+        rotation--;
+    }
+    return turns;
+}
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+        return 1;
+    }
+
+    // Open file
+    FILE *fp = fopen(argv[1], "r");
+    if (fp == NULL) {
+        perror("File does not exist.");
+        return 1;
+    }
+
+    // Allocate malloc
+    dial *d = malloc(sizeof(*d));
+    d->tick = 50;
+    d->len = 100;
+    int zero_count = 0;
+
+    // Read file line by line
+    char buf[8192];
+    while (fgets(buf, sizeof(buf), fp) != NULL) {
+        if (buf[0] == 'L') buf[0] = '-';
+        if (buf[0] == 'R') buf[0] = '+';
+        char *end;
+        int rotation = strtol(buf, &end, 10);
+
+        // Rotate
+        // sleep(1);
+        printf("start SC=%d %d[%d]=", zero_count, d->tick, rotation);
+        int turns = rotate(d, rotation);
+        zero_count += turns;
+        printf("%d - ", d->tick);
+        printf("FC=%d\n", zero_count);
+    }
+
+    printf("END d->tick=%d, d->len=%d, zero_count=%d\n", d->tick, d->len, zero_count);
+}
+
